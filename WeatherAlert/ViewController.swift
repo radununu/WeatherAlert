a81b70c4333ed35ec65779f6bc12baed7721db25
radu.nunu <radu.nunu@yopeso.com>
2016-07-18 17:48:52 +0300

    add single view application

---
+++ WeatherAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAlert
+//
+//  Created by Radu Nunu on 7/18/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
